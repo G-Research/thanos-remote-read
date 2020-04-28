87ee66e8fb15d621b1ba190eca7c7b23d5cd509f
David Leadbeater <dgl@dgl.cx>
2020-04-28 17:51:38 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/G-Research/thanos-remote-read
+
+go 1.13
+
+require (
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/snappy v0.0.1
+	github.com/prometheus/prometheus v1.8.2-0.20200428100226-05038b48bdf0
+	github.com/thanos-io/thanos v0.12.1
+	google.golang.org/grpc v1.29.0
+	google.golang.org/protobuf v1.21.0
+)
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
+	"github.com/thanos-io/thanos/pkg/store/storepb"
+	"google.golang.org/grpc"
+)
+
+var (
+	flagListen = flag.String("listen", ":10080", "[ip]:port to serve HTTP on")
+	flagStore  = flag.String("store", "localhost:10901", "Thanos Store API gRPC endpoint")
+)
+
+func main() {
+	flag.Parse()
+
+	api := &API{}
+	var err error
+	conn, err := grpc.Dial(*flagStore, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.client = storepb.NewStoreClient(conn)
+
+	http.HandleFunc("/api/v1/read", errorWrap(api.remoteRead))
+
+	log.Fatal(http.ListenAndServe(*flagListen, nil))
+}
+
+type API struct {
+	client storepb.StoreClient
+}
+
+func errorWrap(f func(w http.ResponseWriter, r *http.Request) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			if httpErr, ok := err.(HTTPError); ok {
+				http.Error(w, httpErr.Error(), httpErr.Status)
+				return
+			}
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+type HTTPError struct {
+	error
+	Status int
+}
+
+func (api *API) remoteRead(w http.ResponseWriter, r *http.Request) error {
+	compressed, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	reqBuf, err := snappy.Decode(nil, compressed)
+	if err != nil {
+		return HTTPError{err, http.StatusBadRequest}
+	}
+
+	var req prompb.ReadRequest
+	if err := proto.Unmarshal(reqBuf, &req); err != nil {
+		return HTTPError{err, http.StatusBadRequest}
+	}
+
+	resp, err := api.doStoreRequest(r.Context(), &req)
+	if err != nil {
+		return err
+	}
+
+	data, err := proto.Marshal(resp)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/x-protobuf")
+	w.Header().Set("Content-Encoding", "snappy")
+
+	compressed = snappy.Encode(nil, data)
+	if _, err := w.Write(compressed); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+	return nil
+}
+
+var promMatcherToThanos = map[prompb.LabelMatcher_Type]storepb.LabelMatcher_Type{
+	prompb.LabelMatcher_EQ:  storepb.LabelMatcher_EQ,
+	prompb.LabelMatcher_NEQ: storepb.LabelMatcher_NEQ,
+	prompb.LabelMatcher_RE:  storepb.LabelMatcher_RE,
+	prompb.LabelMatcher_NRE: storepb.LabelMatcher_NRE,
+}
+
+func (api *API) doStoreRequest(ctx context.Context, req *prompb.ReadRequest) (*prompb.ReadResponse, error) {
+	response := &prompb.ReadResponse{}
+
+	for _, query := range req.Queries {
+		storeReq := &storepb.SeriesRequest{
+			MinTime: query.StartTimestampMs,
+			MaxTime: query.EndTimestampMs,
+			// Prometheus doesn't understand Thanos compaction, only ask for raw data.
+			Aggregates: []storepb.Aggr{storepb.Aggr_RAW},
+			Matchers:   make([]storepb.LabelMatcher, len(query.Matchers)),
+		}
+		for i, matcher := range query.Matchers {
+			storeReq.Matchers[i] = storepb.LabelMatcher{
+				Name:  matcher.Name,
+				Type:  promMatcherToThanos[matcher.Type],
+				Value: matcher.Value}
+		}
+
+		log.Printf("Thanos request: %v", storeReq)
+		storeRes, err := api.client.Series(ctx, storeReq)
+		if err != nil {
+			return nil, err
+		}
+
+		result := &prompb.QueryResult{}
+		iter := chunkenc.NewNopIterator()
+		for {
+			res, err := storeRes.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("Error in recv from thanos: %v", err)
+				break
+			}
+
+			switch r := res.GetResult().(type) {
+			case *storepb.SeriesResponse_Series:
+				t := &prompb.TimeSeries{}
+				for _, label := range r.Series.Labels {
+					t.Labels = append(t.Labels, prompb.Label{
+						Name: label.Name,
+						Value: label.Value,
+					})
+				}
+
+				for _, chunk := range r.Series.Chunks {
+					if chunk.Raw == nil {
+						// We only ask for and handle RAW
+						log.Printf("Unexpectedly missing raw chunk data")
+						continue
+					}
+					if chunk.Raw.Type != storepb.Chunk_XOR {
+						log.Printf("Unexpected encoding type: %v", chunk.Raw.Type)
+						continue
+					}
+
+					raw, err := chunkenc.FromData(chunkenc.EncXOR, chunk.Raw.Data)
+					if err != nil {
+						log.Printf("Error reading chunk: %v", err)
+						continue
+					}
+
+					iter = raw.Iterator(iter)
+					for iter.Next() {
+						ts, value := iter.At()
+						t.Samples = append(t.Samples, prompb.Sample{
+							Timestamp: ts,
+							Value: value,
+						})
+					}
+				}
+				result.Timeseries = append(result.Timeseries, t)
+
+			case *storepb.SeriesResponse_Warning:
+				// TODO: Can we return this somehow?
+				log.Printf("Warning from thanos: %v", r)
+			}
+		}
+		response.Results = append(response.Results, result)
+	}
+	return response, nil
+}
